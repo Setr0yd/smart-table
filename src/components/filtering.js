cce7580f43abf2bd43af38a9664e5768622cf654
SetrOyd <aponamenko@inbox.ru>
2026-02-03 10:52:51 +0300

    Готов

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,31 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules); 
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
-
+    Object.keys(indexes).forEach((elementName) => {
+        Object.values(indexes[elementName]).map((name) => {
+            const options = document.createAttribute("option");
+            options.value = name;
+            options.textContent = name;
+            return options
+            elements[elementName].append(...options)
+        })
+        
+    }) 
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        if (action && action.dataset.name === "clear") {
+            const parent = action.parentElement;
+            const input = parent.querySelector("input");
+            input.value = "";
+            const field = action.dataset.field;
+            state[field] = '';
+        }
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state)); 
     }
 }
--- src/components/table.js
+++ src/components/table.js
@@ -12,12 +12,59 @@ export function initTable(settings, onAction) {
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach((subName) => {
+        root[subName] = cloneTemplate(subName);
+        root.container.prepend(root[subName].container)
+    })
+
+    after.forEach((subName) => {
+        root[subName] = cloneTemplate(subName);
+        root.container.append(root[subName].container)
+    })
+
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
 
+    root.container.addEventListener("change", (e) => {
+        onAction();
+    })
+
+    root.container.addEventListener("reset", (e) => {
+        setTimeout(onAction, 1000)
+    })
+
+    root.container.addEventListener("submit", (e) => {
+        e.preventDefault()
+        onAction(e.submitter)
+    })
+
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+
+        // мой способ
+        // for (const item of data) {
+        //     const pattern = cloneTemplate(rowTemplate)
+        //     Object.entries(pattern.elements).forEach((cell) => {
+        //         const key = cell[0]
+        //         const element = cell[1]
+
+        //         if (key in item) {
+        //             element.textContent = item[key]; 
+        //         }
+        //     })
+        //     nextRows.push(pattern.container)
+        // }   
+
+        const nextRows = data.map((item) => {
+            const row = cloneTemplate(rowTemplate)
+            Object.keys(item).forEach((key) =>{
+                if(key in row.elements) {
+                    row.elements[key].textContent = item[key];
+                }
+            })
+
+            return row.container
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
